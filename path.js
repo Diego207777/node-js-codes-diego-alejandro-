4366d1a18cafa0fc743c60447fe97c018ee37d82
Diego Alejandro Remote Dev <129339687+Diego207777@users.noreply.github.com>
2023-10-26 23:57:26 +0900

    Create path.js
    codigo cencillo de path module 
    uso simple

---
+++ path.js
@@ -0,0 +1,13 @@
+const path =  require('path ')
+
+console.log('path.sep')
+
+const filePath = path.join('/home/okasamiboku/デスクトップ/proyectos projets ')
+console.log(filePath)
+
+const base = path.basename(filePath)
+
+console.log(base)
+
+
+// code diego alejandro  javascript //
